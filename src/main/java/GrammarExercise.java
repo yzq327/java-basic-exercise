1543d4312cb2d9f04e32c4de606f968891427964
yzq327 <69582746+yzq327@users.noreply.github.com>
2020-09-08 11:01:45 +0800

    Update GrammarExercise.java

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,69 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class GrammarExercise {
-    public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+    public static void main(String[] args) throws RuntimeException{
 
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
+    	Scanner scan =new Scanner(System.in);
+    	while (scan.hasNext()) {
+    		 String firstWordList =scan.next();
+    	     String secondWordList =scan.next();
+    	     if(isMatches(firstWordList) && isMatches(secondWordList)){
+				 List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+				 if(result.size()>0) {
+					 for(int i=0; i<result.size(); i++) {
+						 System.out.print(result.get(i)+" ");
+					 }
+				 }
+			 }else{
+				 throw new RuntimeException("input not valid");
+			 }
+    	}
+    	scan.close();
 
+   }
+
+
+    public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) throws RuntimeException{
+        firstWordList = firstWordList.toLowerCase();
+    	secondWordList = secondWordList.toLowerCase();
+    	List<String> firstList=Arrays.asList(firstWordList.split(","));
+    	List<String> asList=Arrays.asList(secondWordList.split(","));
+		List<String> resultList3 = null;
+    	int cout1=0;
+    	int coun2=0;
+    	for(int i=0;i<firstList.size();i++){
+    		if(!isMatches(firstList.get(i))){
+				cout1=cout1+1;
+			}
+		}
+		for(int i=0;i<asList.size();i++){
+			if(!isMatches(asList.get(i))){
+				coun2=coun2+1;
+			}
+		}
+		if( cout1==0 && coun2==0 ){
+			List<String> resultList1=(List)firstList.stream().filter(it->asList.contains(it)).collect(Collectors.toList());
+
+			List<String> resultList2=(List)resultList1.parallelStream().distinct().collect(Collectors.toList());
+
+			resultList3=(List)resultList2.stream().sorted().collect(Collectors.toList());
+		}else{
+			throw new RuntimeException("input not valid");
+		}
+		return resultList3;
     }
 
-    public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
-    }
+
+    public static boolean isMatches(String wordList){
+		Pattern patt=Pattern.compile("^[A-Za-z]*([A-Za-z]+[,])*([A-Za-z]+)$");
+		//Pattern patt=Pattern.compile("[^a-z]");
+		Matcher match=patt.matcher(wordList);
+		return match.matches();
+	}
 }
